438535c2ab7ac17d4c1c6c942de7d768e565d3e0
Sayed M Rezaie <56697913+cdied@users.noreply.github.com>
2020-11-11 12:59:33 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from tkinter import *
+import pytube
+import subprocess
+
+root = Tk()
+root.title("YouTube Downloader")
+root.geometry("500x250")
+
+
+def download():
+    link = str(entry.get())
+    yt = pytube.YouTube(link)
+    stream = yt.streams.get_highest_resolution()
+    stream.download(".\\Downloads")
+
+    entry.delete(0, END)
+    
+    Label(frame, text="Video Downloaded Succsesfully", background="#1A3D56", foreground="green", font=("Arial", 12)).place(relx=0.5, rely=0.6, anchor=CENTER)
+    button = Button(frame, text="Open in File Explorer", command=lambda:opendir())
+    button.place(relx=0.5, rely=0.8, width=130, height=30, anchor=CENTER)
+
+
+
+def opendir():
+    subprocess.call("explorer .\\Downloads", shell=True)
+
+
+frame = Frame(root, background="#1A3D56", width=5000, height=3200)
+frame.pack(expand=True)
+
+entry_lable = Label(frame, text="Enter a URL", background="#1A3D56", foreground="white")
+entry_lable.place(relx = 0.05, rely = 0.1, height=30)
+
+entry = Entry(frame)
+entry.place(relx = 0.25, rely = 0.1, width=340, height=30)
+
+button = Button(frame, text="Download", command=lambda:download())
+button.place(relx=0.5, rely=0.4, width=100, height=30, anchor=CENTER)
+
+
+root.mainloop()
